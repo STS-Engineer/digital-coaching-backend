e75f9c7c0f0721f23c3b3623400b0f6a3c45d687
STS-Engineer <administration.sts@avocarbon.com>
2026-02-16 15:09:45 +0100

    Update __init__.py

--- bots/__init__.py
+++ bots/__init__.py
@@ -4,21 +4,26 @@ BOTS = {
     "personal": {
         "label": "Personal Problems",
         "runner": personal.run,
+        "runner_stream": personal.run_stream,
     },
     "formalization": {
         "label": "Problem Formalization",
         "runner": formalization.run,
+        "runner_stream": formalization.run_stream,
     },
     "training": {
         "label": "Generic Training",
         "runner": training.run,
+        "runner_stream": training.run_stream,
     },
     "product": {
         "label": "Product Understanding",
         "runner": product.run,
+        "runner_stream": product.run_stream,
     },
     "email": {
         "label": "Email Writer",
         "runner": email.run,
+        "runner_stream": email.run_stream,
     },
 }
